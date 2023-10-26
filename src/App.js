eedf4556b5defc30c21bfdf39c770e7e351ff33a
Alena Sharai <exultantislupus@gmail.com>
2023-10-26 11:33:02 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { initialAnimals } from './data';
 
-function App() {
+function Button({ children }) {
+  return <button className='button'>{children}</button>;
+}
+
+export default function App() {
+  const [animals, setAnimals] = useState(initialAnimals);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className='app'>
+        <div className='sidebar'>
+          <AnimalsList animals={animals} />
+          <FormAddAnimal />
+          <Button>Add animal</Button>
+        </div>
+        <Information animals={animals} />
+      </div>
     </div>
   );
 }
 
-export default App;
+function Header() {
+  return <h1 className='header'>Animal Library</h1>;
+}
+
+function AnimalsList({ animals }) {
+  return (
+    <ul>
+      {animals.map((animal) => (
+        <Animal animal={animal} />
+      ))}
+    </ul>
+  );
+}
+
+function Animal({ animal }) {
+  return (
+    <li>
+      <img src={animal.image} alt={animal.name}></img>
+      <h3>{animal.name}</h3>
+      <Button>Read About</Button>
+    </li>
+  );
+}
+
+function FormAddAnimal() {
+  return (
+    <form className='form-add-animal'>
+      <label>🦁 Animal name</label>
+      <input type='text' />
+
+      <label>🐼 Description</label>
+      <input type='text'></input>
+
+      <label>📸 Image URL</label>
+      <input type='text' />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function Information({ animals }) {
+  return (
+    <div className='info'>
+      <h2>{animals[0].name}</h2>
+      <label>{animals[0].description}</label>
+      <img src={animals[0].image} alt={animals[0].name} />
+    </div>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,25 @@
+export const initialAnimals = [
+  {
+    name: 'Fox',
+    description: `Foxes are small to medium-sized, omnivorous mammals belonging to several genera of the family Canidae. They have a flattened skull, upright, triangular ears, a pointed, slightly upturned snout, and a long bushy tail ("brush").
+    Twelve species belong to the monophyletic "true fox" group of genus Vulpes. Approximately another 25 current or extinct species are always or sometimes called foxes; these foxes are either part of the paraphyletic group of the South American foxes, or of the outlying group, which consists of the bat-eared fox, gray fox, and island fox.
+    Foxes live on every continent except Antarctica. The most common and widespread species of fox is the red fox (Vulpes vulpes) with about 47 recognized subspecies. The global distribution of foxes, together with their widespread reputation for cunning, has contributed to their prominence in popular culture and folklore in many societies around the world. The hunting of foxes with packs of hounds, long an established pursuit in Europe, especially in the British Isles, was exported by European settlers to various parts of the New World.`,
+    image:
+      'https://images.unsplash.com/photo-1474511320723-9a56873867b5?auto=format&fit=crop&q=80&w=2672&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    name: 'Capybara',
+    description: `The capybara or greater capybara (Hydrochoerus hydrochaeris) is a giant cavy rodent native to South America. It is the largest living rodent and a member of the genus Hydrochoerus. The only other extant member is the lesser capybara (Hydrochoerus isthmius). Its close relatives include guinea pigs and rock cavies, and it is more distantly related to the agouti, the chinchilla, and the nutria. The capybara inhabits savannas and dense forests, and lives near bodies of water. It is a highly social species and can be found in groups as large as 100 individuals, but usually live in groups of 10–20 individuals. The capybara is hunted for its meat and hide and also for grease from its thick fatty skin. It is not considered a threatened species.`,
+    image:
+      'https://images.unsplash.com/photo-1662135232446-dd8532117b99?auto=format&fit=crop&q=80&w=2670&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    name: 'Leopard',
+    description: `The leopard (Panthera pardus) is one of the five extant species in the genus Panthera. It has a pale yellowish to dark golden fur with dark spots grouped in rosettes. Its body is slender and muscular reaching a length of 92–183 cm (36–72 in) with a 66–102 cm (26–40 in) long tail and a shoulder height of 60–70 cm (24–28 in). Males typically weigh 30.9–72 kg (68–159 lb), and females 20.5–43 kg (45–95 lb).
+    The leopard was first described in 1758, and several subspecies were proposed in the 19th and 20th centuries. Today, eight subspecies are recognised in its wide range in Africa and Asia. It initially evolved in Africa during the Early Pleistocene, before migrating into Eurasia around the Early–Middle Pleistocene transition. It was present across Europe, but became extinct in the region at the end of the Late Pleistocene, around 11,000 years ago.
+    It is adapted to a variety of habitats ranging from rainforest to steppe, including arid and montane areas. It is an opportunistic predator, hunting mostly ungulates and primates. It relies on its spotted pattern for camouflage as it stalks and ambushes its prey, which it sometimes drags up a tree. It is a solitary animal outside the mating season and when raising cubs. Females usually give birth to a litter of 2–4 cubs once in 15–24 months. Both male and female leopards typically reach sexual maturity at the age 2–2.5 years.
+    It is listed as Vulnerable on the IUCN Red List because leopard populations are threatened by habitat loss and fragmentation, and are declining in large parts of the global range. Leopards have had cultural roles in Ancient Greece, West Africa and modern Western culture. Leopard skins have been popular in fashion.`,
+    image:
+      'https://images.unsplash.com/photo-1566708627877-859df13ae63e?auto=format&fit=crop&q=80&w=2670&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+];
