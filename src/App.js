bb9699825d33521a7d906f9665c0550d4f0d3fe9
Alena Sharai <exultantislupus@gmail.com>
2023-10-26 13:13:44 +0400

    add functionality for adding animals

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import { useState } from 'react';
 import { initialAnimals } from './data';
 
-function Button({ children }) {
-  return <button className='button'>{children}</button>;
+function Button({ children, onClick }) {
+  return (
+    <button onClick={onClick} className='button'>
+      {children}
+    </button>
+  );
 }
 
 export default function App() {
   const [animals, setAnimals] = useState(initialAnimals);
+  const [showAddForm, setShowAddForm] = useState(false);
+
+  function addAnimal(animal) {
+    setAnimals((animals) => [...animals, animal]);
+    setShowAddForm(false);
+  }
+
+  function handleAddAnimalButton() {
+    setShowAddForm((showAddForm) => !showAddForm);
+  }
 
   return (
     <div>
@@ -14,8 +28,10 @@ export default function App() {
       <div className='app'>
         <div className='sidebar'>
           <AnimalsList animals={animals} />
-          <FormAddAnimal />
-          <Button>Add animal</Button>
+          {showAddForm && <FormAddAnimal onAddAnimal={addAnimal} />}
+          <Button onClick={handleAddAnimalButton}>
+            {showAddForm ? 'Close' : 'Add animal'}
+          </Button>
         </div>
         <Information animals={animals} />
       </div>
@@ -31,7 +47,7 @@ function AnimalsList({ animals }) {
   return (
     <ul>
       {animals.map((animal) => (
-        <Animal animal={animal} />
+        <Animal animal={animal} key={animal.id} />
       ))}
     </ul>
   );
@@ -47,19 +63,43 @@ function Animal({ animal }) {
   );
 }
 
-function FormAddAnimal() {
+function FormAddAnimal({ onAddAnimal }) {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [image, setImage] = useState('');
+
+  function handleSumbitAdding(e) {
+    e.preventDefault();
+    if (!name || !description || !image) return;
+
+    const newAnimal = { name, description, image, id: crypto.randomUUID() };
+    onAddAnimal(newAnimal);
+  }
+
   return (
     <form className='form-add-animal'>
       <label>🦁 Animal name</label>
-      <input type='text' />
+      <input
+        type='text'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <label>🐼 Description</label>
-      <input type='text'></input>
+      <input
+        type='text'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
 
       <label>📸 Image URL</label>
-      <input type='text' />
+      <input
+        type='text'
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
 
-      <Button>Add</Button>
+      <Button onClick={handleSumbitAdding}>Add</Button>
     </form>
   );
 }
--- src/data.js
+++ src/data.js
@@ -6,12 +6,14 @@ export const initialAnimals = [
     Foxes live on every continent except Antarctica. The most common and widespread species of fox is the red fox (Vulpes vulpes) with about 47 recognized subspecies. The global distribution of foxes, together with their widespread reputation for cunning, has contributed to their prominence in popular culture and folklore in many societies around the world. The hunting of foxes with packs of hounds, long an established pursuit in Europe, especially in the British Isles, was exported by European settlers to various parts of the New World.`,
     image:
       'https://images.unsplash.com/photo-1474511320723-9a56873867b5?auto=format&fit=crop&q=80&w=2672&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    id: 3587975,
   },
   {
     name: 'Capybara',
     description: `The capybara or greater capybara (Hydrochoerus hydrochaeris) is a giant cavy rodent native to South America. It is the largest living rodent and a member of the genus Hydrochoerus. The only other extant member is the lesser capybara (Hydrochoerus isthmius). Its close relatives include guinea pigs and rock cavies, and it is more distantly related to the agouti, the chinchilla, and the nutria. The capybara inhabits savannas and dense forests, and lives near bodies of water. It is a highly social species and can be found in groups as large as 100 individuals, but usually live in groups of 10–20 individuals. The capybara is hunted for its meat and hide and also for grease from its thick fatty skin. It is not considered a threatened species.`,
     image:
       'https://images.unsplash.com/photo-1662135232446-dd8532117b99?auto=format&fit=crop&q=80&w=2670&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    id: 808983958,
   },
   {
     name: 'Leopard',
@@ -21,5 +23,6 @@ export const initialAnimals = [
     It is listed as Vulnerable on the IUCN Red List because leopard populations are threatened by habitat loss and fragmentation, and are declining in large parts of the global range. Leopards have had cultural roles in Ancient Greece, West Africa and modern Western culture. Leopard skins have been popular in fashion.`,
     image:
       'https://images.unsplash.com/photo-1566708627877-859df13ae63e?auto=format&fit=crop&q=80&w=2670&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    id: 70980985,
   },
 ];
